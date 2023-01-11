24fa93a6bdfbb8297cd12f23354bad06fc6f3bce
d052057 <d052057@yahoo.com>
2023-01-11 14:28:39 -0800

    fix fade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewContainerRef, ViewChild, ComponentFactoryResolver, ComponentRef } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,10 @@ import { Component, ViewContainerRef, ViewChild, ComponentFactoryResolver, Compo
 })
 export class AppComponent {
   title = 'lazycomponent';
-  tag: string[] = ['Album'];
+  tag: string[] = ['Album','Carousel'];
   @ViewChild('example', { read: ViewContainerRef })
   viewContainer: ViewContainerRef | any;
+  componentRef!: ComponentRef<any>;
   constructor(
     private viewContainerRef: ViewContainerRef,
     private componentFactoryResolver: ComponentFactoryResolver
@@ -17,12 +19,33 @@ export class AppComponent {
   };
   ngOnInit(): void {
   }
-  async getExample(value: string) {
+  fadeout(): void {
+    if (this.componentRef) {
+      this.componentRef.location.nativeElement.classList.add('fadeMeOut');
+    }
+  }
 
-    this.viewContainer.clear();
+  async getExample(value: string) {
+    //const animation = (value: string, callback: any): any => {
+    //  this.loadObject(value);
+    //  callback();
+      
+    //};
+    //animation(value, this.fadeout());
+    
+    // this must be done first
+    if (this.componentRef) {
+      this.componentRef.location.nativeElement.classList.add('fadeMeOut');
+      await this.timeout(1000);
+    }
+    // this must be done second
     switch (value) {
       case this.tag[0]: {
         await this.getAlbum();
+        break;
+      }
+      case this.tag[1]: {
+        await this.getCarousel();
         break;
       }
       default: {
@@ -31,9 +54,19 @@ export class AppComponent {
       }
     }
   }
+  timeout(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
   async getAlbum() {
     const { AlbumComponent } = await import("src/app/album/album.component");
-    const componentRef:ComponentRef<any> = this.viewContainer.createComponent(AlbumComponent);
-    componentRef.location.nativeElement.classList.add('fade');
+    this.viewContainer.clear();
+    this.componentRef = this.viewContainer.createComponent(AlbumComponent);
+    this.componentRef.location.nativeElement.classList.add('fadeMeIn');
+  }
+  async getCarousel() {
+    const { CarouselComponent } = await import("src/app/carousel/carousel.component");
+    this.viewContainer.clear();
+    this.componentRef = this.viewContainer.createComponent(CarouselComponent);
+    this.componentRef.location.nativeElement.classList.add('fadeMeIn');
   }
 }
